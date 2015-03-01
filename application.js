a1bde706018c9b7ad6865d34c4ceb16b6e499d38
realalexhomer <realalexhomer@gmail.com>
2015-02-28 23:45:34 -0500

    adds more readable function for widget

--- application.js
+++ application.js
@@ -1,22 +1,37 @@
 // Model
 
-function markdownWidget(input, preview){
+function hideousMarkdownWidget(input, preview){
 
   for (var i = 0; i < arguments.length; i++) {
-   arguments[i] = $(arguments[i]);
-  }
+   arguments[i] = $( arguments[i] );
+  };
 
-  input.keyup(function(){
-    preview.html(markdown.toHTML(input.val()))
+  input.keyup(function () {
+    preview.html(markdown.toHTML(input.val()));
   });
 
-}
+
+
+ }
+
+// Better:
+
+function markdownWidget(input, preview) {
+
+  $( input ).keyup(function () {
+    $( preview ).html(
+      markdown.toHTML(
+        $( input ).val()
+      )
+    );
+  });
+
+};
 
 // Controller
 
 
-$( document ).ready(function (){
-
+$( document ).ready(function () {
 
   markdownWidget("#markdown", "#preview");
 
